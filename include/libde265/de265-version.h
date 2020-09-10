c27a24ab4b2b495b9f224ac445351060ba81ca83
DreamPiggy <lizhuoli1126@126.com>
2020-09-10 11:43:11 +0800

    Bumped version to 1.0.6

--- include/libde265/de265-version.h
+++ include/libde265/de265-version.h
@@ -28,9 +28,9 @@
 #define LIBDE265_VERSION_H
 
 /* Numeric representation of the version */
-#define LIBDE265_NUMERIC_VERSION 0x01000500
+#define LIBDE265_NUMERIC_VERSION 0x01000600
 
 /* Version string */
-#define LIBDE265_VERSION "1.0.5"
+#define LIBDE265_VERSION "1.0.6"
 
 #endif
